8b0c5981be0e594a746403bf0699525efe3ec7f1
sulenurgul <sulenurr.gul@gmail.com>
2026-09-08 22:33:29 +0300

    Initial commit: Angular 22 project setup

---
+++ src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withNoIncrementalHydration,
+} from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay(), withNoIncrementalHydration()),
+  ],
+};
